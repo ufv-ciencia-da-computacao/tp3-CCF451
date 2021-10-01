51f67f93be941947255dc3fb9c4e412d403cce1c
Dener Vieira <deneribeiro10@hotmail.com>
2021-10-01 01:04:22 -0300

    add ls and cd

---
+++ src/cmd_if.c
@@ -0,0 +1,143 @@
+#include "../lib/cmd_if.h"
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void cmd_mkdir(int argc, char *argv[]) {
+    char opt;
+    char *name;
+    while((opt = getopt(argc, argv, ":")) != -1) {
+        switch(opt) {
+            default:
+                return;
+        }
+    }
+    
+    if(argc - optind == 0) {
+        fprintf(stderr, "%s: faltando operando\n", argv[0]);
+        return;
+    }
+
+    for(; optind < argc; ++optind) {
+        name = argv[optind];
+        printf("Criar dir %s\n", name);
+    }
+    
+}
+
+void cmd_rm(int argc, char *argv[]) {
+    char opt;
+    int remove_dir = 0;
+    char *name = NULL;
+    while((opt = getopt(argc, argv, "r")) != -1) {
+        switch(opt) {
+            case 'r':
+                remove_dir = 1;
+                break;
+            default:
+                return;
+        }
+    }
+    if(argc - optind == 0) {
+        fprintf(stderr, "%s: faltando operando\n", argv[0]);
+        return;
+    }
+
+    for(; optind < argc; ++optind) {
+        name = argv[optind];
+        if(remove_dir) {
+            printf("Remove dir %s\n", name);
+        } else {
+            printf("Remove file %s\n", name);
+        }
+    }
+
+}
+
+void cmd_ls(int argc, char *argv[]) {
+    char opt;
+    char *name = NULL;
+    int print_inode = 0;
+    while((opt = getopt(argc, argv, "rl")) != -1) {
+        switch(opt) {
+            case 'i':
+                print_inode = 1;
+                break;
+            case 'l':
+                break;
+            default:
+                return;
+        }
+    }
+
+    if(optind == argc) {
+        printf("list here\n");
+    } else {
+        for(; optind < argc; ++optind) {
+            printf("list dir %s\n", argv[optind]);
+        }
+    }
+
+}
+
+void cmd_cd(int argc, char *argv[]) {
+    char opt;
+    char name[28] = "/";
+    while((opt = getopt(argc, argv, ":")) != -1) {
+        switch(opt) {
+            default:
+                printf("%s: argumetos invalidos\n", argv[0]);
+                return;
+        }
+    }
+
+    if(argc - optind >= 2) {
+        printf("%s: muitos argumentos\n", argv[0]);
+        return;
+    }
+
+    if(argc - optind == 1) {
+        strcpy(name, argv[optind]);
+    }
+
+    printf("open dir %s\n", name);
+}
+
+int get_cmd_value(char cmd[]) {
+    const char *commands[] = {"exit", "mkdir", "rm", "ls", "clear", "cd"};
+    const int cntCommands = 6;
+
+    for(int i=0; i<cntCommands; ++i) {
+        if(strcmp(cmd, commands[i]) == 0) return i;
+    }
+    return -1;
+}
+
+void cmd_execute(int argc, char *argv[]) {
+    optind = 0;
+    switch(get_cmd_value(argv[0])) {
+        case 0:
+            exit(0);
+            break;
+        case 1: {
+            cmd_mkdir(argc, argv);
+            break;
+        }
+        case 2:
+            cmd_rm(argc, argv);
+            break;
+        case 3:
+            cmd_ls(argc, argv);
+            break;
+        case 4:
+            system("clear");
+            break;
+        case 5:
+            cmd_cd(argc, argv);
+            break;
+
+        default:
+            printf("%s: comando nao encontrado\n", argv[0]);
+    }
+}
