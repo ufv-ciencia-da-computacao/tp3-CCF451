a2cfa1910e2e7ef7ccd79e6ec3bdab1be91b36c2
Dener Vieira <deneribeiro10@hotmail.com>
2021-10-01 01:06:02 -0300

    upd cmd_if.h

--- lib/cmd_if.h
+++ lib/cmd_if.h
@@ -1,10 +1,6 @@
 #ifndef __COMMANDS_H__
 #define __COMMANDS_H__
 
-void cmd_mkdir(int argc, char *argv[]);
-void cmd_rm(int argc, char *argv[]);
-void cmd_ls(int argc, char *argv[]);
 void cmd_execute(int argc, char *argv[]);
-void cmd_cd(int argc, char *argv[]);
 
 #endif
