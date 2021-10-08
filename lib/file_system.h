cf453e031d84e41ce1120a52df4a58cc54c4ffff
Dener Vieira <deneribeiro10@hotmail.com>
2021-10-08 12:54:28 -0300

    fs_create done

--- lib/file_system.h
+++ lib/file_system.h
@@ -3,6 +3,7 @@
 
 #include <stdio.h>
 #include "disk.h"
+#include "bitmap.h"
 
 #define IF_DIR          1
 #define IF_FILE         2
@@ -11,14 +12,17 @@
 typedef struct {
     disk_t *disk;
     int blocks_bitmap_inode;
-    int blocks_bitmap_data;
+    int blocks_bitmap_block;
     int blocks_inode;
     int blocks_data;
+    bitmap_t b_inode;
+    bitmap_t b_block;
 } file_system_t;
 
 void fs_init(file_system_t *fs, disk_t *disk);
+void fs_flush(file_system_t *fs);
+void fs_destroy(file_system_t *fs);
 void fs_format(file_system_t *fs);
-
 int  fs_create(file_system_t *fs, int file_type);
 int  fs_read(file_system_t *fs, int inode_index, uint8_t *data);
 void fs_write(file_system_t *fs, int inode_index, uint8_t *data, int size);
